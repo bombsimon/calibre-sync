85859251c3e8b76a8558df0324522fa0fbefc26d
Simon Sawert <simon@sawert.se>
2020-01-28 00:17:42 +0100

    Fix weird leading whitespace and permission

--- sync.sh
+++ sync.sh
@@ -1,24 +1,24 @@
- #!/usr/bin/env sh
+#!/usr/bin/env sh
 
- set -eu
+set -eu
 
- sync() {
-     rsync -av --delete "$1" "$2"
- }
+sync() {
+    rsync -av --delete "$1" "$2"
+}
 
- CALIBRE_SOURCE="$HOME/Calibre Library/"
- BACKUP_DESTINATION="/Volumes/DATA/books/00.calibre/"
+CALIBRE_SOURCE="$HOME/Calibre Library/"
+BACKUP_DESTINATION="/Volumes/DATA/books/00.calibre/"
 
- printf "Will mirror all the data, including removing files in source.\n"
- printf "Source:      %s\n" "$CALIBRE_SOURCE"
- printf "Destination: %s\n" "$BACKUP_DESTINATION"
- printf "%s\n" "-"
+printf "Will mirror all the data, including removing files in source.\n"
+printf "Source:      %s\n" "$CALIBRE_SOURCE"
+printf "Destination: %s\n" "$BACKUP_DESTINATION"
+printf "%s\n" "-"
 
- printf "Are you sure? [y/N] "
- read -r areyousure
+printf "Are you sure? [y/N] "
+read -r areyousure
 
- if [ "$areyousure" = "y" ]; then
-     sync "$CALIBRE_SOURCE" "$BACKUP_DESTINATION"
- else
-     printf "Aborting...\n"
- fi 
+if [ "$areyousure" = "y" ]; then
+    sync "$CALIBRE_SOURCE" "$BACKUP_DESTINATION"
+else
+    printf "Aborting...\n"
+fi
