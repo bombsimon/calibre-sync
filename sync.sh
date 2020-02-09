9b9aafd3305e81d621a6cb280e353eadb110081d
Simon Sawert <simon@sawert.se>
2020-02-09 17:50:49 +0100

    Support swap direction in sh version

--- sync.sh
+++ sync.sh
@@ -1,24 +1,60 @@
 #!/usr/bin/env sh
 
-set -eu
+set -u
+
+MASTER="/Volumes/DATA/books/00.calibre/"
+SLAVE="$HOME/Calibre Library/"
 
 sync() {
     rsync -av --delete "$1" "$2"
 }
 
-MASTER="/Volumes/DATA/books/00.calibre/"
-SLAVE="$HOME/Calibre Library/"
+swap() {
+    OLD_MASTER="$MASTER"
+    OLD_SLAVE="$SLAVE"
 
-printf "Will mirror all the data, including removing files in source.\n"
-printf "Source:      %s\n" "$MASTER"
-printf "Destination: %s\n" "$SLAVE"
-printf "%s\n" "-"
+    MASTER="$OLD_SLAVE"
+    SLAVE="$OLD_MASTER"
+}
 
-printf "Are you sure? [y/N] "
-read -r areyousure
+show_sync() {
+    printf "Will mirror all the data, including removing files in source.\n"
+    printf "Source:      %s\n" "$MASTER"
+    printf "Destination: %s\n" "$SLAVE"
+    printf "%s\n" "-"
+}
 
-if [ "$areyousure" = "y" ]; then
-    sync "$MASTER" "$SLAVE"
-else
-    printf "Aborting...\n"
-fi
+show_prompt() {
+    printf "%s" "$1"
+    read -r areyousure
+
+
+    if [ "$areyousure" = "y" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+while true; do
+    show_sync
+    show_prompt "Are you sure? [y/N] "
+
+    do_sync=$?
+
+    if [ "$do_sync" -eq 0 ]; then
+        sync "$MASTER" "$SLAVE"
+        exit
+    fi
+
+    show_prompt "Swap direction? [y/N] "
+    do_swap=$?
+
+    if [ "$do_swap" -ne 0 ]; then
+        printf "Ok, aborting...\n"
+        exit
+    fi
+
+    swap
+    printf "\n\n%s\n\n" "-"
+done
